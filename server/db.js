8556b9aa1be2b9046c02ce3fc2f8f0036b49a76d
Anjaya Induwara <136043121+anjaya02@users.noreply.github.com>
2025-08-19 12:49:01 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,10 @@
-import mongoose from 'mongoose';
-import { config } from './config.js';
+import mongoose from "mongoose";
+import { config } from "./config.js";
 
 export async function connectDB() {
+  if (!config.mongoUri) {
+    throw new Error("MONGO_URI is not set");
+  }
   await mongoose.connect(config.mongoUri);
-  console.log('Mongo connected');
+  console.log("Mongo connected");
 }
